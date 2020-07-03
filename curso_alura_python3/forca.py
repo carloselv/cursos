b1de2e7d8c9eec807acaf38ea91c7b83abf32e0a
Carlos Valente <cadu.valente@gmail.com>
2020-07-03 14:14:21 -0300

    continuacao

--- curso_alura_python3/forca.py
+++ curso_alura_python3/forca.py
@@ -4,6 +4,38 @@ def jogar():
     print("Bem vindo ao jogo de Forca      ")
     print("********************************")
 
+    palavra_secreta = "banana".upper()
+    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+
+    print(letras_acertadas)
+
+    enforcou = False
+    acertou = False
+    erros = 0
+
+    while(not enforcou and not acertou):
+
+        tentativa = input("Qual a letra?")
+        tentativa = tentativa.strip().upper()
+
+        if(tentativa in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(tentativa == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1        
+
+        acertou = "_" not in letras_acertadas
+        enforcou = erros == 6      
+        print(letras_acertadas)
+        
+
+    if(acertou):
+        print("Voce Ganhou!!!")
+    else:
+        print("Voce perdeu!!")
     print("Fim do jogo!")
 
 
