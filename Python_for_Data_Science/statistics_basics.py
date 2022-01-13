78cb1d03e29b198d01dc0094d5f31477eaba55d7
Carlos Eduardo Lemos Valente <carlos-eduardo.valente@serpro.gov.br>
2022-01-13 09:11:06 -0300

    Inclusao de artefato - esboco inicial

---
+++ Python_for_Data_Science/statistics_basics.py
@@ -0,0 +1,36 @@
+
+
+    1. Descriptive Statistics (mean,mode,mdedian)
+    2. Variability (standard deviation, variance, range, percentile, quartile, interquartile range)
+    3. Correlation (correlation coefficient)
+    4. Probability Distribution (independent event, dependent event)
+    5. Regression (Linear regression, logistic regression)
+    6. Normal Distribution
+    7. Bias
+    8. Central limit theorem (CLT)
+    3. Dimensionality Reduction
+    4. Central Tendency
+    5. Hypothesis Testing
+    6. Tests of significance
+    7. Sampling theory
+    8. Bayesian Statistics
+
+
+#https://www.kdnuggets.com/2020/06/8-basic-statistics-concepts.html
+
+dataset = [18, 24, 67, 55, 42, 14, 19, 26, 33]
+
+#Média (mean) - Mean: The average of the dataset.
+mean(dataset)
+
+#Mediana (median) - Median: The middle value of an ordered dataset.
+
+#variância (variance)
+
+#Desvio Padrão (standard deviation)
+
+#Moda () - Mode: The most frequent value in the dataset. If the data have multiple values that occurred the most frequently, we have a multimodal distribution.
+
+#Skewness: A measure of symmetry.
+
+#Kurtosis: A measure of whether the data are heavy-tailed or light-tailed relative to a normal distribution
